32169504940e3d6d4fca330ad012ec02cff5e473
Claire Muller <clairermuller@gmail.com>
2021-12-07 23:22:22 -0800

    reload or next page

--- last-fm-remove-duplicates.js
+++ last-fm-remove-duplicates.js
@@ -5,8 +5,18 @@ var names = elements.map(function (element) {
   return nameElement && nameElement.textContent.replace(/\s+/g, ' ').trim();
 })
 
+var count = 0;
+
 names.forEach(function (name, i) {
   if (name !== names[i + 1]) return;
   var deleteButton = elements[i].querySelector('[data-ajax-form-sets-state="deleted"]');
   if (deleteButton) deleteButton.click();
+  count += 1;
 })
+
+if (count > 0) {
+  location.reload();
+} else {
+  var next = document.querySelector('.pagination-next').querySelector('a');
+  next.click();
+}
